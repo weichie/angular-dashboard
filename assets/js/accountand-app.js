1d278e40f837f49f3f2a61e6b54e2e2167784552
Bob Weichler <bobmasaki@hotmail.com>
2016-05-27 18:39:34 +0200

    Angular setup + ngRotue

--- assets/js/accountand-app.js
+++ assets/js/accountand-app.js
@@ -1,5 +1,44 @@
-var app = angular.module('accountand-app', []);
+var app = angular.module('accountand-app', ['ngRoute']);
 
-app.controller('MainCtrl', ['$scope', function($scope){
-	$scope.test = "Mojo";
+app.config(function($routeProvider){
+	$routeProvider
+		.when('/', {
+			templateUrl: 'pages/home.html',
+			controller: 'MainCtrl'
+		});
+		/*
+		.when('/about', {
+            templateUrl : 'pages/about.html',
+            controller  : 'aboutController'
+        })
+        .when('/contact', {
+            templateUrl : 'pages/contact.html',
+            controller  : 'contactController'
+        });
+		*/
+});
+
+app.factory('inkomsten', [function(){
+	var o = {
+		inkomsten: []
+	};
+	return o;
+}]);
+
+app.controller('MainCtrl', ['$scope', 'inkomsten', function($scope, inkomsten){
+	$scope.inkomsten = inkomsten.inkomsten;
+
+	$scope.addRekening = function(){
+		if(!$scope.factuur || $scope.factuur == "" || !$scope.bedrag || $scope.bedrag == ""){
+			return;
+		}
+
+		$scope.inkomsten.push({
+			factuur: $scope.factuur, 
+			bedrag: $scope.bedrag
+		});
+
+		$scope.factuur = "";
+		$scope.bedrag = "";
+	};
 }]);
