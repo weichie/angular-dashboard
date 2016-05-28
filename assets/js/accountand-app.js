537564ee0d0f6f916229f1537e466a820b16aec9
Bob Weichler <bobmasaki@hotmail.com>
2016-05-28 08:53:19 +0200

    Updated sidebar & Controller verkopen / Aankopen

--- assets/js/accountand-app.js
+++ assets/js/accountand-app.js
@@ -18,17 +18,25 @@ app.config(function($routeProvider){
 		*/
 });
 
-app.factory('inkomsten', [function(){
+app.factory('verkopen', [function(){
 	var o = {
 		inkomsten: []
 	};
 	return o;
 }]);
 
-app.controller('MainCtrl', ['$scope', 'inkomsten', function($scope, inkomsten){
-	$scope.inkomsten = inkomsten.inkomsten;
+app.factory('aankopen', [function(){
+	var o = {
+		kosten: []
+	};
+	return o;
+}])
 
-	$scope.addRekening = function(){
+app.controller('MainCtrl', ['$scope', 'verkopen', 'aankopen', function($scope, verkopen, aankopen){
+	$scope.inkomsten = verkopen.inkomsten;
+	$scope.kosten = aankopen.kosten;
+
+	$scope.addInkomsten = function(){
 		if(!$scope.factuur || $scope.factuur == "" || !$scope.bedrag || $scope.bedrag == ""){
 			return;
 		}
@@ -40,5 +48,19 @@ app.controller('MainCtrl', ['$scope', 'inkomsten', function($scope, inkomsten){
 
 		$scope.factuur = "";
 		$scope.bedrag = "";
+	};
+
+	$scope.addKosten = function(){
+		if(!$scope.rekening || $scope.rekening == "" || !$scope.kost || $scope.kost == ""){
+			return;
+		}
+
+		$scope.kosten.push({
+			rekening: $scope.rekening,
+			kost: $scope.kost
+		});
+
+		$scope.rekening = "";
+		$scope.kost = "";
 	};
 }]);
